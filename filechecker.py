7736a9ba6eb0f7acaa7fa9241e8b6114540b770a
Lliye <adampaluba19@gmail.com>
2024-08-02 17:41:19 +0200

    Added the requested changes

--- filechecker.py
+++ filechecker.py
@@ -10,8 +10,15 @@ def checkpath(path):
     if os.path.exists(path):
         print("Valid path!")
         return path
+    elif os.path.isdir(path):
+        raise argparse.ArgumentTypeError(f"This is a directory")
     else:
         raise argparse.ArgumentTypeError(f"Not a valid path")
 
 
-parsed_args = parse_arguments()
+
+def main():
+    parsed_args = parse_arguments()
+
+if __name__ == "__main__":
+    main()
