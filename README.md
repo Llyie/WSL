70d4186d07438ea4c19db0a5fb0cb1615821ec26
Lliye <adampaluba19@gmail.com>
2024-09-02 22:24:00 +0200

    Reads file metadata

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # WSL
-# Text file information reader 
+## Text File Information Reader
+
 This program will display the following information about a given text file:
- * [ ] File size
- * [ ] Number of characters
- * [ ] Creation date 
- * [ ] Last modification date
-<br>
-<strong>To achieve this, we will need to do the following steps</strong>
-* Implement reading parameters from the command line - we need to read filepath
-* Read file metadata - we need the file creation date, file modification date, the file size</em>
-* Read file contents - number of characters, maybe we could read the amount of words in the future as well
+
+- [ ] File size
+- [ ] Number of characters
+- [ ] Creation date
+- [ ] Last modification date
+
+**To achieve this, we will need to do the following steps:**
+
+- Implement reading parameters from the command line - we need to read the filepath.
+- Read file metadata - we need the file creation date, file modification date, and file size.
+- Read file contents - number of characters (maybe we could read the number of words in the future as well).
--- filechecker.py
+++ filechecker.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import datetime
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -7,11 +8,22 @@ def parse_arguments():
     return parser.parse_args()
 
 def checkpath(path):
-    if os.path.exists(path):
-        print("Valid path!")
-        return path
-    elif os.path.isdir(path):
-        raise argparse.ArgumentTypeError(f"This is a directory")
+    if os.path.isdir(path):
+        raise argparse.ArgumentTypeError(f"This is a directory")    
+    elif os.path.exists(path):        
+        file_size = os.path.getsize(path)
+        file_creation_time = datetime.datetime.fromtimestamp(os.path.getctime(path))
+        file_modification_time = datetime.datetime.fromtimestamp(os.path.getmtime(path))
+        
+        print(f"File Size = {file_size} bytes")
+        print(f"File Creation Time = {file_creation_time}")
+        print(f"File Modification Time = {file_modification_time}")
+        
+        with open(path) as reader:
+            char = reader.read()
+            print(char)
+            print(len(char))
+            return path
     else:
         raise argparse.ArgumentTypeError(f"Not a valid path")
 
@@ -22,3 +34,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
